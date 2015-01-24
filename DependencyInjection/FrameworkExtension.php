7c61d21b9e6ba84d84df1ca7739b84b4cd1368ca
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2015-01-24 21:24:11 +0200

    Initial commit for framework bundle

---
+++ DependencyInjection/FrameworkExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the OXID package.
+ *
+ * (c) Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EllisV\Oxid\Bundle\FrameworkBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * FrameworkExtension.
+ */
+class FrameworkExtension extends Extension
+{
+    /**
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ FrameworkBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the OXID package.
+ *
+ * (c) Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EllisV\Oxid\Bundle\FrameworkBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FrameworkBundle extends Bundle
+{
+}
